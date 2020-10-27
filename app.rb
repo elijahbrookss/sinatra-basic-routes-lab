1092670c68e6dfdb8ecb63fb6c6adad1884cc652
elijahbrookss <elijahcbrooks@gmail.com>
2020-10-27 13:54:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Elijah"
+  end
+
+  get '/hometown' do
+    "My hometown is Sacramento"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Emotionally Scarred - Lil Baby"
+  end
+
 end
